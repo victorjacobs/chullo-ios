a91e2ad384bc6ba0c09a54d20d01d8c1929b0f45
Victor Jacobs <victor.jacobs@me.com>
2016-06-18 20:50:44 +0200

    Initial Commit

---
+++ Chullo/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Chullo
+//
+//  Created by Victor Jacobs on 18/06/16.
+//  Copyright © 2016 Victor Jacobs. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
